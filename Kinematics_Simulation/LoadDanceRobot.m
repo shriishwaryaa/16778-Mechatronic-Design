cec2e8af15ae82e205103bd9bbc2cbe12023699c
byte125 <sdeepak@andrew.cmu.edu>
2024-03-16 19:01:38 -0400

    Add rigid body tree for final robot and Matlab script to load parameter values

---
+++ Kinematics_Simulation/LoadDanceRobot.m
@@ -0,0 +1,39 @@
+hip_link_width = 0.2286;
+hip_link_length = 0.1;
+hip_link_height = 0.1;
+
+knee_link_short_width = 0.0762;
+knee_link_short_length = 0.1;
+knee_link_short_height = 0.1;
+
+knee_link_long_width = 0.05;
+knee_link_long_length = 0.1;
+knee_link_long_height = 0.6096;
+
+base_width = 0.6096;
+base_length = 0.6096;
+base_height = 0.2;
+
+plane_length = 3;
+plane_width = 50;
+plane_height = 0.5;
+
+foot_length = 0.1;
+foot_width = 0.1;
+foot_height = 0.15;
+
+% foot_height + knee_link_long_height + base_height/2 
+base_offset = 0.8096; % Height of the foot plus height of the knee link
+
+% knee_link_short_width/2 + knee_link_long_width/2
+knee_short_to_long_x = 0.0881;
+
+% knee_link_long_height/2 - knee_link_short_height/2
+knee_short_to_long_z = 0.3548;
+
+% knee_link_long_height/2 + foot_height/2
+% right_knee_to_foot_z = 0.3548;
+right_knee_to_foot_z = 0.3798;
+
+
+
