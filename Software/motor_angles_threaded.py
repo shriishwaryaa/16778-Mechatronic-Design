03fd7f401c57fa2a2f3486db42b77065261703b7
Shreya <sdeepak@andrew.cmu.edu>
2024-04-22 00:13:01 -0400

    Test same serial port for continuous motion

--- Software/motor_angles_threaded.py
+++ Software/motor_angles_threaded.py
@@ -1,11 +1,39 @@
 import threading
 import serial
 import time
-
+import csv
+import math
 threads = []
+filename = 'output_joint_data.csv'
+ard1 = "/dev/ttyACM0"
+# ard2 = "/dev/arduino_1"
 
-ard1 = "/dev/arduino_0"
-ard2 = "/dev/arduino_1"
+def find_row_by_first_column(filename, search_value):
+    with open(filename, mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            # Check if the first column matches the search value
+            if row[0] == search_value:
+                # Convert the entire row to float where possible
+                float_row = []
+                for item in row:
+                    try:
+                        # Attempt to convert each item to a float
+                        float_row.append(float(item))
+                    except ValueError:
+                        # If conversion fails, keep the original item
+                        float_row.append(item)
+                return float_row
+    return None
+
+result_row0 = find_row_by_first_column(filename, '6')
+result_row1 = find_row_by_first_column(filename, '7')
+
+if result_row0 and result_row1:
+    print("Found row with float conversion where possible:", result_row0, result_row1)
+else:
+    print("No row found with that value.")
+
 
 ser1 = None
 ser2 = None
@@ -13,8 +41,8 @@ ser2 = None
 def calculate_delay(angle):
   # 5 degrees to 2 seconds
   # angle is in degrees
-  delay = (abs(angle) // 10) * 2
-  return delay
+  # delay = (abs(angle) // 10) * 2
+  return 1
 
 def l1_thread():
   global ser1
@@ -61,22 +89,18 @@ def main():
   global ser2
   ser1 = serial.Serial(ard1, 9600, timeout=1)
   ser1.reset_input_buffer()
-  ser2 = serial.Serial(ard2, 9600, timeout=1)
-  ser2.reset_input_buffer()
 
-  l1 = threading.Thread(target=l1_thread)
-  l1.start()
-  threads.append(l1)
+  for i in range(len(result_row0)):
+    angle=math.degrees(result_row0[i])
+    ser1.write((str(angle) + "$" + str(0) + '\n').encode('utf-8'))
 
-  l2 = threading.Thread(target=l2_thread)
-  l2.start()
-  threads.append(l2)
+    # send one line for all motors 
+    # angle1$0$angle2$1$angle3$2$angle4
 
-  for t in threads:
-    t.join()
-
-  ser1.close()
-  ser2.close()
+    time.sleep(0.1)
+    angle=math.degrees(result_row0[i])
+    ser1.write((str(angle) + "$" + str(1) + '\n').encode('utf-8'))
+    time.sleep(0.)
 
   print("BYE maga")
 
